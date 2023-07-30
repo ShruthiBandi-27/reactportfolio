bee34f0d3c0170775fb0357ff6fcea245a1042e6
ShruthiBandi-27 <shruthi27998@gmail.com>
2023-07-30 14:49:29 +0530

    commit

---
+++ src/routes/Home.js
@@ -0,0 +1,16 @@
+import Footer from "../components/Footer";
+import MainImg from "../components/MainImg";
+import Navbar from "../components/Navbar";
+import '../components/WorkCard.css';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+            <MainImg/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Home
